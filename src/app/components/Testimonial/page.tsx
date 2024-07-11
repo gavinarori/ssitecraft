79ef8ebb7e78716d2d5ae82918600d62cecf98b7
gavinarori <arorigavin@gmail.com>
2024-07-11 22:11:44 +0300

    added:animations

--- src/app/components/Testimonial/page.tsx
+++ src/app/components/Testimonial/page.tsx
@@ -3,7 +3,7 @@
 import React, { useEffect, useState } from "react";
 import { InfiniteMovingCards } from "../ui/infinite-moving-cards";
 
-export function InfiniteMovingCardsDemo() {
+function InfiniteMovingCardsDemo() {
   return (
     <div className="h-[40rem] rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
       <InfiniteMovingCards
@@ -14,6 +14,8 @@ export function InfiniteMovingCardsDemo() {
     </div>
   );
 }
+
+export default InfiniteMovingCardsDemo;
 
 const testimonials = [
   {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Announcement from '@/app/components/Announcement/page'
 import  Landing  from "./components/landing/page";
 import  Services  from "./components/services/page";
 import  Footer  from "./components/Footer/page";
-import  {InfiniteMovingCardsDemo}  from "./components/Testimonial/page"; 
+import  InfiniteMovingCardsDemo  from "./components/Testimonial/page"; 
 import  {MacbookScrollDemo} from "./components/Stats";
 import  Extra  from "./components/Extra";
 import  FAQs  from "./components/FAQs";
