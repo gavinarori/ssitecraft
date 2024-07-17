85e6ed78edc4004fd88f1ecf6c9bd9883ac3418a
gavinarori <arorigavin@gmail.com>
2024-07-17 11:06:25 +0300

    update(booking button):with a modal and form

--- src/app/components/header/page.tsx
+++ src/app/components/header/page.tsx
@@ -22,6 +22,15 @@ import {
   XMarkIcon,
 } from '@heroicons/react/24/outline'
 
+import {
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalTrigger,
+} from "../ui/animated-modal";
+import Image from "next/image";
+import { motion } from "framer-motion";
 
 
 
@@ -31,6 +40,15 @@ function classNames(...classes: string[]) {
 
 export default function Example() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+  const images = [
+    "https://img.freepik.com/free-photo/close-up-image-programer-working-his-desk-office_1098-18707.jpg?t=st=1721200257~exp=1721203857~hmac=112d651e368fd37c021df296b04364574d57ac5bf5a3727e0205c529011a7f85&w=740",
+    "https://img.freepik.com/free-photo/web-design-technology-browsing-programming-concept_53876-163260.jpg?t=st=1721200174~exp=1721203774~hmac=45533563117ee8bde18b872fdc21216a0a4473be195f41477bdec95a39a94689&w=740",
+    "/wetoger_1.jpeg",
+    "/wetogether.jpeg",
+    "https://img.freepik.com/free-photo/web-design-concepts-with-blurred-background_1134-82.jpg?t=st=1721200327~exp=1721203927~hmac=e0b9b1e7bfdf1410581ca68eea58b68fe9f6ef40ddce1a817c5c83b16b56b17e&w=740",
+  ];
+
 
   return (
     <header className="">
@@ -61,18 +79,103 @@ export default function Example() {
         </PopoverGroup>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
         
-      <button
-                type="submit"
+     
+              <Modal>
+        <ModalTrigger className=" flex justify-center group/modal-btn">
+        <span
                 className="flex-none rounded-md bg-indigo-500 text-white px-3.5 py-2.5 text-sm font-semibold  shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
               >
                 Book with us
-              </button>
+              </span>
+
+        </ModalTrigger>
+        <ModalBody>
+          <ModalContent>
+            <h4 className="text-lg md:text-2xl text-neutral-600 dark:text-neutral-100 font-bold text-center mb-8">
+            Get a Website that Meets Your Expectations with {" "}
+              <span className="px-1 py-0.5 rounded-md bg-gray-100 dark:bg-neutral-800 dark:border-neutral-700 border border-gray-200">
+              Our Expert Web Development
+              </span>{" "}
+              Services! 
+            </h4>
+            <div className="flex justify-center items-center">
+              {images.map((image, idx) => (
+                <motion.div
+                  key={"images" + idx}
+                  style={{
+                    rotate: Math.random() * 20 - 10,
+                  }}
+                  whileHover={{
+                    scale: 1.1,
+                    rotate: 0,
+                    zIndex: 100,
+                  }}
+                  whileTap={{
+                    scale: 1.1,
+                    rotate: 0,
+                    zIndex: 100,
+                  }}
+                  className="rounded-xl -mr-4 mt-4 p-1 bg-white dark:bg-neutral-800 dark:border-neutral-700 border border-neutral-100 flex-shrink-0 overflow-hidden"
+                >
+                  <Image
+                    src={image}
+                    alt="bali images"
+                    width="500"
+                    height="500"
+                    className="rounded-lg h-20 w-20 md:h-40 md:w-40 object-cover flex-shrink-0"
+                  />
+                </motion.div>
+              ))}
+            </div>
+            <div className="py-10 flex flex-wrap gap-x-4 gap-y-6 items-start justify-start max-w-sm mx-auto">
+              <div className="flex  items-center justify-center">
+                <PlaneIcon className="mr-1 text-neutral-700 dark:text-neutral-300 h-4 w-4" />
+                <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+                Cost-effective solutions
+                </span>
+              </div>
+              <div className="flex items-center justify-center">
+                <ElevatorIcon className="mr-1 text-neutral-700 dark:text-neutral-300 h-4 w-4" />
+                <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+                technologies and trends
+                </span>
+              </div>
+              <div className="flex items-center justify-center">
+                <VacationIcon className="mr-1 text-neutral-700 dark:text-neutral-300 h-4 w-4" />
+                <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+                project management
+                </span>
+              </div>
+              <div className="flex  items-center justify-center">
+                <FoodIcon className="mr-1 text-neutral-700 dark:text-neutral-300 h-4 w-4" />
+                <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+                visibility and promotion
+                </span>
+              </div>
+              <div className="flex items-center justify-center">
+                <MicIcon className="mr-1 text-neutral-700 dark:text-neutral-300 h-4 w-4" />
+                <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+                Progress communication
+                </span>
+              </div>
+            </div>
+          </ModalContent>
+          <ModalFooter className="gap-4">
+            <button className="px-2 py-1 bg-gray-200 text-black dark:bg-black dark:border-black dark:text-white border border-gray-300 rounded-md text-sm w-28">
+              Cancel
+            </button>
+            <a href='https://forms.gle/bGaxqr7fvHddxxVi9' className="bg-black text-white dark:bg-white dark:text-black text-sm px-2 py-1 text-center rounded-md border border-black w-28">
+              Book Now
+            </a>
+          </ModalFooter>
+        </ModalBody>
+      </Modal>
 
         </div>
       </nav>
       <Dialog className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-10" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto  px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white  px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
@@ -105,12 +208,96 @@ export default function Example() {
               </div>
               <div className="py-6">
               
-      <button
-                type="submit"
+              <Modal>
+        <ModalTrigger className=" flex justify-center group/modal-btn">
+        <span
                 className="flex-none rounded-md bg-indigo-500 text-white px-3.5 py-2.5 text-sm font-semibold  shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
               >
-              Book with us
-              </button>
+                Book with us
+              </span>
+
+        </ModalTrigger>
+        <ModalBody>
+          <ModalContent>
+          <h4 className="text-lg md:text-2xl text-neutral-600 dark:text-neutral-100 font-bold text-center mb-8">
+            Get a Website that Meets Your Expectations with {" "}
+              <span className="px-1 py-0.5 rounded-md bg-gray-100 dark:bg-neutral-800 dark:border-neutral-700 border border-gray-200">
+              Our Expert Web Development
+              </span>{" "}
+              Services! 
+            </h4>
+            <div className="flex justify-center items-center">
+              {images.map((image, idx) => (
+                <motion.div
+                  key={"images" + idx}
+                  style={{
+                    rotate: Math.random() * 20 - 10,
+                  }}
+                  whileHover={{
+                    scale: 1.1,
+                    rotate: 0,
+                    zIndex: 100,
+                  }}
+                  whileTap={{
+                    scale: 1.1,
+                    rotate: 0,
+                    zIndex: 100,
+                  }}
+                  className="rounded-xl -mr-4 mt-4 p-1 bg-white dark:bg-neutral-800 dark:border-neutral-700 border border-neutral-100 flex-shrink-0 overflow-hidden"
+                >
+                  <Image
+                    src={image}
+                    alt="bali images"
+                    width="500"
+                    height="500"
+                    className="rounded-lg h-20 w-20 md:h-40 md:w-40 object-cover flex-shrink-0"
+                  />
+                </motion.div>
+              ))}
+            </div>
+            <div className="py-10 flex flex-wrap gap-x-4 gap-y-6 items-start justify-start max-w-sm mx-auto">
+              <div className="flex  items-center justify-center">
+                <PlaneIcon className="mr-1 text-neutral-700 dark:text-neutral-300 h-4 w-4" />
+                <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+                Cost-effective solutions
+                </span>
+              </div>
+              <div className="flex items-center justify-center">
+                <ElevatorIcon className="mr-1 text-neutral-700 dark:text-neutral-300 h-4 w-4" />
+                <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+                technologies and trends
+                </span>
+              </div>
+              <div className="flex items-center justify-center">
+                <VacationIcon className="mr-1 text-neutral-700 dark:text-neutral-300 h-4 w-4" />
+                <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+                project management
+                </span>
+              </div>
+              <div className="flex  items-center justify-center">
+                <FoodIcon className="mr-1 text-neutral-700 dark:text-neutral-300 h-4 w-4" />
+                <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+                visibility and promotion
+                </span>
+              </div>
+              <div className="flex items-center justify-center">
+                <MicIcon className="mr-1 text-neutral-700 dark:text-neutral-300 h-4 w-4" />
+                <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+                Progress communication
+                </span>
+              </div>
+            </div>
+          </ModalContent>
+          <ModalFooter className="gap-4">
+            <button className="px-2 py-1 bg-gray-200 text-black dark:bg-black dark:border-black dark:text-white border border-gray-300 rounded-md text-sm w-28">
+              Cancel
+            </button>
+            <a href='https://forms.gle/bGaxqr7fvHddxxVi9' className="bg-black text-white text-center dark:bg-white dark:text-black text-sm px-2 py-1 rounded-md border border-black w-28">
+              Book Now
+            </a>
+          </ModalFooter>
+        </ModalBody>
+      </Modal>
 
               </div>
             </div>
@@ -120,3 +307,62 @@ export default function Example() {
     </header>
   )
 }
+
+
+const PlaneIcon = ({ className }: { className?: string }) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
+  <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z" />
+</svg>
+
+  );
+};
+ 
+const VacationIcon = ({ className }: { className?: string }) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75" />
+  </svg>
+  
+  );
+};
+ 
+const ElevatorIcon = ({ className }: { className?: string }) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
+  <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18 9 11.25l4.306 4.306a11.95 11.95 0 0 1 5.814-5.518l2.74-1.22m0 0-5.94-2.281m5.94 2.28-2.28 5.941" />
+</svg>
+
+  );
+};
+ 
+const FoodIcon = ({ className }: { className?: string }) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
+  <path strokeLinecap="round" strokeLinejoin="round" d="m6.115 5.19.319 1.913A6 6 0 0 0 8.11 10.36L9.75 12l-.387.775c-.217.433-.132.956.21 1.298l1.348 1.348c.21.21.329.497.329.795v1.089c0 .426.24.815.622 1.006l.153.076c.433.217.956.132 1.298-.21l.723-.723a8.7 8.7 0 0 0 2.288-4.042 1.087 1.087 0 0 0-.358-1.099l-1.33-1.108c-.251-.21-.582-.299-.905-.245l-1.17.195a1.125 1.125 0 0 1-.98-.314l-.295-.295a1.125 1.125 0 0 1 0-1.591l.13-.132a1.125 1.125 0 0 1 1.3-.21l.603.302a.809.809 0 0 0 1.086-1.086L14.25 7.5l1.256-.837a4.5 4.5 0 0 0 1.528-1.732l.146-.292M6.115 5.19A9 9 0 1 0 17.18 4.64M6.115 5.19A8.965 8.965 0 0 1 12 3c1.929 0 3.716.607 5.18 1.64" />
+</svg>
+
+  );
+};
+ 
+const MicIcon = ({ className }: { className?: string }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M15 12.9a5 5 0 1 0 -3.902 -3.9" />
+      <path d="M15 12.9l-3.902 -3.899l-7.513 8.584a2 2 0 1 0 2.827 2.83l8.588 -7.515z" />
+    </svg>
+  );
+};
+ 
