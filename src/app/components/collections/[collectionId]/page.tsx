03137e48db57356a48fc324b0ad125a1df4886ac
gavinarori <arorigavin@gmail.com>
2024-07-19 13:33:01 +0300

    paypal payment

--- src/app/components/collections/[collectionId]/page.tsx
+++ src/app/components/collections/[collectionId]/page.tsx
@@ -1,59 +1,81 @@
 "use client"
 
-const products = [
-    {
-      id: 1,
-      title: "Goldfinch",
-      price: "$20,000",
-      imgSrc: "https://uploads-ssl.webflow.com/6637f8a2ca48c8a9c81c0dfe/6637f8a2ca48c8a9c81c0ea7_goldfinch-thumb.webp"
-    },
-    {
-      id: 2,
-      title: "content creators website",
-      price: "Personal time",
-      imgSrc: "https://uploads-ssl.webflow.com/6637f8a2ca48c8a9c81c0dfe/6637f8a2ca48c8a9c81c0ea5_Course%20Template.webp"
-    },
-    {
-      id: 3,
-      title: "Lumio Homepage",
-      price: "$20,000",
-      imgSrc: "https://uploads-ssl.webflow.com/6637f8a2ca48c8a9c81c0dfe/6637f8a2ca48c8a9c81c0ea3_Lumio%20Homepage.webp"
-    },
-    {
-      id: 4,
-      title: "food catering",
-      price: "£24.00 GBP",
-      imgSrc: "https://uploads-ssl.webflow.com/6637f8a2ca48c8a9c81c0dfe/663904668f8979077697f0fd_Untitled%20design%20(1).png"
-    },
-    {
-      id: 5,
-      title: "Deadfellaz Website",
-      price: "private",
-      imgSrc: "https://uploads-ssl.webflow.com/6637f8a2ca48c8a9c81c0dfe/6637f8a2ca48c8a9c81c0e8a_Frame%2036-min.png"
-    },
-    {
-      id: 6,
-      title: "creators Homepage",
-      price: "$5000",
-      imgSrc: "https://uploads-ssl.webflow.com/6637f8a2ca48c8a9c81c0dfe/6638fc5db95cf1175bd394ef_Untitled%20design.png"
-    }
-    ,
-    {
-      id: 7,
-      title: "Warbler Labs",
-      price: "$20,000",
-      imgSrc: "https://uploads-ssl.webflow.com/6637f8a2ca48c8a9c81c0dfe/6637f8a2ca48c8a9c81c0e66_Warbler.png"
-    },
-    {
-      id: 8,
-      title: "Planto",
-      price: "private",
-      imgSrc: "https://uploads-ssl.webflow.com/6637f8a2ca48c8a9c81c0dfe/6639fcdbca63788dd58cfa85_Untitled%20design%20(2).png"
-    }
-  ];
+import { useState } from 'react';
+import { PayPalScriptProvider, PayPalButtons } from '@paypal/react-paypal-js'
+import axios from 'axios';
 
+const products = [
+  {
+    id: 1,
+    title: "Goldfinch",
+    price: 20000,
+    imgSrc: "https://uploads-ssl.webflow.com/6637f8a2ca48c8a9c81c0dfe/6637f8a2ca48c8a9c81c0ea7_goldfinch-thumb.webp"
+  },
+  {
+    id: 2,
+    title: "content creators website",
+    price: 0,
+    imgSrc: "https://uploads-ssl.webflow.com/6637f8a2ca48c8a9c81c0dfe/6637f8a2ca48c8a9c81c0ea5_Course%20Template.webp"
+  },
+  {
+    id: 3,
+    title: "Lumio Homepage",
+    price: 20000,
+    imgSrc: "https://uploads-ssl.webflow.com/6637f8a2ca48c8a9c81c0dfe/6637f8a2ca48c8a9c81c0ea3_Lumio%20Homepage.webp"
+  },
+  {
+    id: 4,
+    title: "food catering",
+    price: 24,
+    imgSrc: "https://uploads-ssl.webflow.com/6637f8a2ca48c8a9c81c0dfe/663904668f8979077697f0fd_Untitled%20design%20(1).png"
+  },
+  {
+    id: 5,
+    title: "Deadfellaz Website",
+    price: 0,
+    imgSrc: "https://uploads-ssl.webflow.com/6637f8a2ca48c8a9c81c0dfe/6637f8a2ca48c8a9c81c0e8a_Frame%2036-min.png"
+  },
+  {
+    id: 6,
+    title: "creators Homepage",
+    price: 5000,
+    imgSrc: "https://uploads-ssl.webflow.com/6637f8a2ca48c8a9c81c0dfe/6638fc5db95cf1175bd394ef_Untitled%20design.png"
+  }
+  ,
+  {
+    id: 7,
+    title: "Warbler Labs",
+    price: 20000,
+    imgSrc: "https://uploads-ssl.webflow.com/6637f8a2ca48c8a9c81c0dfe/6637f8a2ca48c8a9c81c0e66_Warbler.png"
+  },
+  {
+    id: 8,
+    title: "Planto",
+    price: 0,
+    imgSrc: "https://uploads-ssl.webflow.com/6637f8a2ca48c8a9c81c0dfe/6639fcdbca63788dd58cfa85_Untitled%20design%20(2).png"
+  }
+];
 
 const ProductDetails = ({ params }:any) => {
+   
+  const paypalCreateOrder = async (data: any, actions: any) => {
+    return actions.order.create({
+        purchase_units: [
+            {
+                amount: {
+                    value: product?.price.toString() || "0",
+                },
+            },
+        ],
+    });
+};
+const paypalCaptureOrder = async (data: any, actions: any) => {
+  return actions.order.capture().then(function (details: any) {
+      console.log('Transaction completed by ' + details.payer.name.given_name);
+      // Call your server to save the transaction
+      
+  });
+};
 
   const { collectionId } = params;
   const product = products.find(p => p.id === parseInt(collectionId));
@@ -66,14 +88,14 @@ const ProductDetails = ({ params }:any) => {
   <div className="xl:w-2/6 lg:w-2/5 w-80 md:block ">
   <div className="flex flex-wrap gap-6 pb-3">
             <a
-              href="/components/collections"
+              href=""
               className="relative flex bg-slate-800 h-12 w-full items-center rounded-full justify-center px-8 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max"
             >
               <span className="relative text-base font-semibold text-white "
                 >Preview</span>
             </a>
             <a
-              href="/components/collections"
+              href=""
               className="relative flex h-12 w-full items-center justify-center px-8 before:absolute before:inset-0 before:rounded-full before:border before:border-gray-200 before:bg-gray-50 before:bg-gradient-to-b before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 dark:before:border-gray-700 dark:before:bg-gray-800 sm:w-max"
             >
               <span
@@ -100,9 +122,24 @@ const ProductDetails = ({ params }:any) => {
         </svg>
       </div>
     </div>
-    <button className="dark:bg-white dark:text-gray-900 dark:hover:bg-gray-100 rounded-lg focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 text-base flex items-center justify-center leading-none text-white bg-gray-800 w-full py-4 hover:bg-gray-700 focus:outline-none">
-      Check availability in store
-    </button>
+    <PayPalScriptProvider
+                    options={{
+                        clientId: "AdS6sUVrAXflGHAMp9tbj8Ye8PpFK1rCenAW_l2d7b5-scdqZL-jUH0lTxBbGOhbEGLJzGHCItlofM1E",
+                        currency: 'USD',
+                        intent: 'capture'
+                    }}
+                >
+                    <PayPalButtons
+                        style={{
+                            color: 'gold',
+                            shape: 'rect',
+                            label: 'pay',
+                            height: 50
+                        }}
+                        createOrder={paypalCreateOrder}
+                        onApprove={paypalCaptureOrder}
+                    />
+                </PayPalScriptProvider>
     <div>
       <div className="border-t border-b py-4 mt-7 border-gray-200">
         <div data-menu className="flex justify-between items-center cursor-pointer">
