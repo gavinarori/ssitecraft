e9952b53ad76500d635375f176b4fa9048d08c54
gavinarori <arorigavin@gmail.com>
2024-07-24 15:06:11 +0300

    added:download page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,7 @@ module.exports = {
     // rest of the code
     extend: {
       animation: {
+        orbit: "orbit calc(var(--duration)*1s) linear infinite",
         scroll:
           "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
           aurora: "aurora 60s linear infinite",
@@ -20,6 +21,16 @@ module.exports = {
           "marquee-vertical": "marquee-vertical var(--duration) linear infinite",
       },
       keyframes: {
+        orbit:{
+          "0%": {
+            transform:
+              "rotate(0deg) translateY(calc(var(--radius) * 1px)) rotate(0deg)",
+          },
+          "100%": {
+            transform:
+              "rotate(360deg) translateY(calc(var(--radius) * 1px)) rotate(-360deg)",
+          },
+        },
         marquee: {
           from: { transform: "translateX(0)" },
           to: { transform: "translateX(calc(-100% - var(--gap)))" },
