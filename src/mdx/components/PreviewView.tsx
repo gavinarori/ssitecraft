c5d3dcaa2329af48eb2b867239c28dc96d83875b
Gavin Arori <99688077+gavinarori@users.noreply.github.com>
2024-09-29 11:15:15 +0300

    Mdx (#58)
    * mdx components
    
    * final refactor

--- src/mdx/components/PreviewView.tsx
+++
@@ -1,9 +0,0 @@
-import ButtonStyle from './ButtonStyle'
-
-export default function PreviewView({ showPreview, handleSetShowPreview }:any) {
-  return (
-    <button onClick={() => handleSetShowPreview(!showPreview)}>
-      <ButtonStyle buttonEmoji="👀" buttonText="View" buttonActive={!showPreview} />
-    </button>
-  )
-}
